4e1ba2bf5366e8b71839fe85b984ef4be97b150d
Kacper Dural <kdural2@illinois.edu>
2023-10-15 15:12:44 -0500

    Recognizes Coursera

--- src/frontend/popup.tsx
+++ src/frontend/popup.tsx
@@ -1,29 +1,50 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [currentUrl, setCurrentUrl] = useState<string>("")
+  const getCurrentUrl = async () => {
+    const [tab] = await chrome.tabs.query({active: true, currentWindow: true})
+    setCurrentUrl(tab.url)
+  }
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
-    </div>
-  )
+  useEffect(() => {
+    getCurrentUrl()
+  }, [currentUrl])
+  
+  if (currentUrl.includes("coursera.org")) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 16
+        }}>
+        <h2>
+          Welcome to CourseBuddy!
+        </h2>
+        <a href="https://docs.plasmo.com" target="_blank">
+          View Docs
+        </a>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          padding: 16
+        }}>
+        <h2>
+          This extension only works with Coursera!
+        </h2>
+        <a href="https://docs.plasmo.com" target="_blank">
+          View Docs
+        </a>
+      </div>
+    )
+  }
 }
 
 export default IndexPopup
