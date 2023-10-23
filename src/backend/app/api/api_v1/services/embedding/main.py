8089d122342bf8fc17b517d15acc99f8b9212d3f
Avinash B. <abaldeo@users.noreply.github.com>
2023-10-23 06:07:38 +0000

    fixed embedding main.py

--- src/backend/app/api/api_v1/services/embedding/main.py
+++ src/backend/app/api/api_v1/services/embedding/main.py
@@ -5,7 +5,6 @@ from app.core import config
 from app.db.session import SessionLocal
 from app.core.auth import get_current_active_user
 from service import router 
-from fastapi.responses import ORJSONResponse
 
 app = FastAPI(
     title=config.settings.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api", default_response_class=ORJSONResponse
