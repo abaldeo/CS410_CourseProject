87bee796675224d709d017cc810fa9a7a1ff8bee
Avinash B. <abaldeo@users.noreply.github.com>
2023-10-28 07:15:06 +0000

    use orjson

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -14,8 +14,11 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
   
 
+from fastapi.responses import ORJSONResponse
+  
+
 app = FastAPI(
-    title=config.settings.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api", default_response_class=ORJSONResponse
+    title=config.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api", default_response_class=ORJSONResponse
 )
 
 
