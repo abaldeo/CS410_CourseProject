73ff9dc9a46db26439f7a58138baf19bd208570b
Avinash B. <abaldeo@users.noreply.github.com>
2023-10-29 23:46:12 +0000

    add method for llm call  cost

--- src/backend/app/api/api_v1/services/embedding/token_count.py
+++ src/backend/app/api/api_v1/services/embedding/token_count.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tiktoken
 from loguru import logger
 
@@ -34,19 +33,12 @@ def llm_call_cost(response):
     completion_token_cost = (usage["completion_tokens"] * completion_cost)/1000
     return prompt_token_cost + completion_token_cost
 
-=======
-import tiktoken 
->>>>>>> bc1f253 (changes for embedding service)
 
 def get_encoder_for_model(model_name: str): 
     try:
         encoding = tiktoken.encoding_for_model(model_name)
     except KeyError:
-<<<<<<< HEAD
         # logger.info("Warning: model not found. Using cl100k_base encoding.")
-=======
-        # print("Warning: model not found. Using cl100k_base encoding.")
->>>>>>> bc1f253 (changes for embedding service)
         encoding = tiktoken.get_encoding("cl100k_base")
     return encoding 
 
@@ -58,19 +50,19 @@ def num_tokens_from_string(string: str, model_name: str) -> int:
  
 
 def compare_encodings(example_string: str) -> None:
-    """Prints a comparison of three string encodings."""
-    # print the example string
-    print(f'\nExample string: "{example_string}"')
-    # for each encoding, print the # of tokens, the token integers, and the token bytes
+    """logger.infos a comparison of three string encodings."""
+    # logger.info the example string
+    logger.info(f'\nExample string: "{example_string}"')
+    # for each encoding, logger.info the # of tokens, the token integers, and the token bytes
     for encoding_name in ["r50k_base", "p50k_base", "cl100k_base"]:
         encoding = tiktoken.get_encoding(encoding_name)
         token_integers = encoding.encode(example_string)
         num_tokens = len(token_integers)
         token_bytes = [encoding.decode_single_token_bytes(token) for token in token_integers]
-        print()
-        print(f"{encoding_name}: {num_tokens} tokens")
-        print(f"token integers: {token_integers}")
-        print(f"token bytes: {token_bytes}")
+        logger.info()
+        logger.info(f"{encoding_name}: {num_tokens} tokens")
+        logger.info(f"token integers: {token_integers}")
+        logger.info(f"token bytes: {token_bytes}")
         
 def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
     """Return the number of tokens used by a list of messages."""
@@ -89,10 +81,10 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
         tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
         tokens_per_name = -1  # if there's a name, the role is omitted
     elif "gpt-3.5-turbo" in model:
-        print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        logger.info("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
         return num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613")
     elif "gpt-4" in model:
-        print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        logger.info("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
         return num_tokens_from_messages(messages, model="gpt-4-0613")
     else:
         raise NotImplementedError(
@@ -107,4 +99,3 @@ def num_tokens_from_messages(messages, model="gpt-3.5-turbo-0613"):
                 num_tokens += tokens_per_name
     num_tokens += 3  # every reply is primed with <|start|>assistant<|message|>
     return num_tokens
-
