361c317e139f9642abcf0a25d1531799f33632c4
zcpohl2 <144085530+zcpohl2@users.noreply.github.com>
2023-10-29 22:21:09 +0000

    Added docstrings

--- src/backend/app/api/api_v1/services/summarization/core.py
+++ src/backend/app/api/api_v1/services/summarization/core.py
@@ -20,6 +20,14 @@ class SummaryRequestModel(BaseModel):
     S3Path: str
 
 def get_transcript_from_s3(s3_path: str) -> List[Document]:
+    """Retrieves the text transcript of a video from s3
+
+    Args:
+        s3_path (str): Amazon s3 path to access transcript 
+
+    Returns:
+        List[Document]: List of one Document object that is the transcript for a specific video
+    """
     parsed_s3 = urlparse(url=s3_path, allow_fragments=False)
     parsed_s3.path.lstrip('/')  # Internet implies this is generally a problem will need to verify
     loader = S3FileLoader(bucket=parsed_s3.netloc, key=parsed_s3.path, 
@@ -28,19 +36,50 @@ def get_transcript_from_s3(s3_path: str) -> List[Document]:
     try:
         return loader.load()
     except ClientError as error:
-        # Handles if the s3 path dne
+        # Handles if the s3 path DNE
         return None
 
 def check_cache(course_name: str, video_name: str) -> dict:
+    """_summary_
+
+    Args:
+        course_name (str): course video is associated with
+        video_name (str): video's transcript that's being summarized
+
+    Returns:
+        dict: Returns summary and metadata 
+    """
     return {"courseName": course_name, "videoName": video_name, "summary": "summary"}
 
 def save_to_cache(course_name: str, video_name: str, summary: str):
-    pass
+     """Saves generated summary to a cache for easy access
 
-def generate_summary(txt_to_summarize: Document):
+     Args:
+        course_name (str): course video is associated with
+        video_name (str): video's transcript that's being summarized
+        summary (str): Summary of transcript
+     """
+     pass
+
+def generate_summary(txt_to_summarize: Document) -> str:
+    """Uses an LLM to generate a summary for a transcript
+
+    Args:
+        txt_to_summarize (Document): The transcript to summarize
+
+    Returns:
+        str: Returns summarization of the transcript
+    """
     llm = openai()
     summary_chain = load_summarize_chain(llm=llm, chain_type="map_reduce")
     return summary_chain.run(txt_to_summarize)
 
-def write_summary_to_db():
+def write_summary_to_db(course_name: str, video_name: str, summary: str) -> None:
+    """Writes the generated summary to a database
+
+    Args:
+        course_name (str): course video is associated with
+        video_name (str): video's transcript that's being summarized
+        summary (str): Summary of transcript
+    """
     pass
