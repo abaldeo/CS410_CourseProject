44370999fe2414848c1c77be5ff86759f902ea3f
Avinash B. <abaldeo@users.noreply.github.com>
2023-10-31 09:19:07 +0000

    refactor code to use config.settings

--- src/backend/app/core/celery_app.py
+++ src/backend/app/core/celery_app.py
@@ -1,11 +1,6 @@
 from celery import Celery
-from dotenv import load_dotenv
-import os
+from app.core import config
 
-load_dotenv()
-
-CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL", "redis://redis:6379/0")
-
-celery_app = Celery("worker", broker=CELERY_BROKER_URL)
+celery_app = Celery("worker", broker=config.settings.CELERY_BROKER_URL)
 
 celery_app.conf.task_routes = {"app.tasks.*": "main-queue"}
