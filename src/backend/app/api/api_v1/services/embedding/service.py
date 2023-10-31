f79ea4f548da41449db9a6a3fb7d0db27162ad05
Avinash B. <abaldeo@users.noreply.github.com>
2023-10-31 09:19:07 +0000

    refactor code to use config.settings

--- src/backend/app/api/api_v1/services/embedding/service.py
+++ src/backend/app/api/api_v1/services/embedding/service.py
@@ -14,43 +14,46 @@ from app.api.api_v1.services.embedding.core import (get_token_splitter,
 from app.api.api_v1.services.embedding.utils import Timer 
 from app.api.api_v1.services.embedding.token_count import num_tokens_from_string
 
-from langchain.storage.upstash_redis import UpstashRedisStore
+# from langchain.storage.upstash_redis import UpstashRedisStore
+# from upstash_redis import Redis
+import functools
 from langchain.storage.redis import RedisStore
 
-from langchain.embeddings import CacheBackedEmbeddings
-from upstash_redis import Redis
-import redis
+# from dotenv import load_dotenv
+# import os 
+from pydantic import BaseModel
+from app.core.config import settings, get_settings
 
 
-
-load_dotenv()
+# load_dotenv()
 
 router = r = APIRouter()
 
-REDIS_URL = os.getenv("UPSTASH_REDIS_REST_URL")
-REDIS_TOKEN = os.getenv("UPSTASH_REDIS_REST_TOKEN")
-# print(REDIS_URL)
-# print(REDIS_TOKEN)
-redis_client = Redis(url=REDIS_URL, token=REDIS_TOKEN, rest_retries=5, rest_retry_interval=3, allow_telemetry=False)
-redis_instance = redis.Redis(
-  host='us1-viable-civet-41613.upstash.io',
-  port=41613,
-  password='63099f9a0987407a9fb00fb8890707d3'
-)
+REDIS_URL = settings.EMBEDDING_REDIS_URL
+REDIS_TOKEN = settings.EMBEDDING_REDIS_TOKEN
+REDIS_HOST = settings.EMBEDDING_REDIS_HOST
+REDIS_PORT = settings.EMBEDDING_REDIS_PORT 
+REDIS_PASSWD = settings.EMBEDDING_REDIS_PASSWD
 
 
-REDIS_STORE  = RedisStore(client=redis_instance, ttl=None, namespace="embedding_service")
+@functools.lru_cache()
+def get_redis_instance():
+    # redis_client = Redis(url=REDIS_URL, token=REDIS_TOKEN, rest_retries=5, rest_retry_interval=3, allow_telemetry=False)    
+    redis_client = redis.Redis( host=REDIS_HOST, port=REDIS_PORT,password=REDIS_PASSWD)
+    return redis_client
 
-MODEL_NAME = os.getenv("MODEL_NAME")
-S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
-EMBEDDING_MODEL_NAME = os.getenv("EMBEDDING_MODEL_NAME")
-EMEDDING_MODEL  = get_embedding_model(EMBEDDING_MODEL_NAME)
-EMBEDDING_CACHE = UpstashRedisStore(client=redis_client, ttl=None, namespace="embedding_service")
+redis_instance = get_redis_instance()
+REDIS_STORE  = RedisStore(client= redis_instance, ttl=None, namespace="embedding_service")
+
+GPT_MODEL_NAME = settings.GPT_MODEL_NAME
+S3_BUCKET_NAME = settings.S3_BUCKET_NAME
+EMEDDING_MODEL  = get_embedding_model(settings.EMBEDDING_MODEL_NAME)
+# EMBEDDING_CACHE = UpstashRedisStore(client=redis_client, ttl=None, namespace="embedding_service")
 
 EMBEDDER = CacheBackedEmbeddings.from_bytes_store(
     underlying_embeddings=EMEDDING_MODEL, 
     document_embedding_cache=REDIS_STORE, 
-    namespace=MODEL_NAME
+    namespace=GPT_MODEL_NAME
 )
 
 VECTOR_DB =  create_vectorstore(embedding_model=EMBEDDER)
@@ -64,7 +67,7 @@ def compute_query_embedding(query_text: str):
     # model = EMEDDING_MODEL
     with Timer() as t:
         vectors = create_query_embeddings(query_text, EMBEDDER)
-    token_count = num_tokens_from_string(query_text, MODEL_NAME)
+    token_count = num_tokens_from_string(query_text, GPT_MODEL_NAME)
     output = {"text": query_text, "embedding": vectors, "token_count": token_count, "time_taken": t.elapsed() }
     return output 
 
@@ -76,6 +79,7 @@ def store_document_embedding(S3Path:str):
     chunks = chunk_docs(document, text_splitter)
     # model = get_embedding_model()
     # model  = EMEDDING_MODEL
+    token_count = sum([num_tokens_from_string(doc.page_content, GPT_MODEL_NAME) for doc in chunks])
     with Timer() as t:
         vectors = create_doc_embeddings(chunks, EMBEDDER)
     VECTOR_DB.add_documents(chunks)
@@ -98,6 +102,9 @@ def store_text_embedding(request: Item):
     text = request.text
     text_splitter = get_text_splitter()
     chunks = chunk_texts(text, text_splitter)
+    for chunk in chunks:
+        chunk.metadata['source'] = "user_input"
+    token_count = sum([num_tokens_from_string(chunk.page_content, GPT_MODEL_NAME) for chunk in chunks])        
     with Timer() as t:
         vectors = create_text_embeddings(chunks, EMBEDDER)
     token_count = sum([num_tokens_from_string(chunk, MODEL_NAME) for chunk in chunks])
