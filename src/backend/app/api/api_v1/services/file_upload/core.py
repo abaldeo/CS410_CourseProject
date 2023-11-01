3dfbba5151420a7fbf1bec13386fcdb9cda95e79
Colton Bailey <coltonbailey32@gmail.com>
2023-11-01 01:03:09 +0000

    upload slides

--- src/backend/app/api/api_v1/services/file_upload/core.py
+++ src/backend/app/api/api_v1/services/file_upload/core.py
@@ -2,7 +2,7 @@ import boto3
 import botocore
 import os
 import requests
-
+import pathlib
 
 # Helper function to upload a transcript
 def upload_transcript(course_name, transcript_name, transcript_text):
@@ -48,6 +48,63 @@ def retrieve_transcript(course_name, transcript_name):
                     object_key,
                     './object_key')
     return "File downloaded successfully!"
+
+
+# Helper function to upload a slide deck (.ppt, .pdf)
+def upload_slides(course_name, transcript_name, slide_object):
+    session = boto3.session.Session()
+    client = session.client('s3',
+                            # Configures to use subdomain/virtual calling format.
+                            config=botocore.config.Config(s3={'addressing_style': 'virtual'}), 
+                            region_name='nyc3',
+                            endpoint_url='https://nyc3.digitaloceanspaces.com',
+                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+    # Define the object key (file path)
+    object_key = f"{course_name}/{slide_object}"
+
+    # Get file extension to ensure it is correct format
+    file_extension = pathlib.Path(slide_object).suffix
+
+    if file_extension in [".pdf", ".ppt"]:
+        # Upload the transcript text to DigitalOcean Spaces
+        client.put_object(Bucket="coursebuddy",
+                    Key=object_key,
+                    Body=slide_object,
+                    ACL='private',
+                    Metadata={
+                        'x-amz-meta-my-key': 'placeholder-value'
+                    }
+        )
+        return f"Slide deck {slide_object} for course {course_name} uploaded successfully."
+    else:
+        return f"Unsupported file format: {file_extension}. Slide deck not uploaded."
+
+
+# Helper function to retrieve a transcript
+def retrieve_slides(course_name, transcript_name):
+    # Define the object key (file path)
+    object_key = f"{course_name}/{transcript_name}.txt"
+    session = boto3.session.Session()
+    client = session.client('s3',
+                             # Configures to use subdomain/virtual calling format.
+                            config=botocore.config.Config(s3={'addressing_style': 'virtual'}),
+                            region_name='nyc3',
+                            endpoint_url='https://nyc3.digitaloceanspaces.com',
+                            aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                            aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+
+    #Retrieve the transcript text from DigitalOcean Spaces
+    transcript_text = client.download_file('coursebuddy',
+                    object_key,
+                    './object_key')
+    return "File downloaded successfully!"
+
+
+
+
+
 
 
 #Uploads all text files in a directory 
