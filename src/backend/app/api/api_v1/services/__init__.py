9ebbe72c84ce9275c6b8e68d6b71daf37769972d
Colton Bailey <coltonbailey32@gmail.com>
2023-11-08 01:44:45 +0000

    doesnt work anymore :)

--- src/backend/app/api/api_v1/services/__init__.py
+++ src/backend/app/api/api_v1/services/__init__.py
@@ -4,8 +4,9 @@ from .embedding import embedding_router
 
 from .summarization import summarization_router
 
+from .file_upload import file_upload_router
 
 service_router = APIRouter()
 service_router.include_router(embedding_router)
 service_router.include_router(summarization_router)
-
+service_router.include_router(file_upload_router)
--- src/backend/app/api/api_v1/services/file_upload/main.py
+++ src/backend/app/api/api_v1/services/file_upload/main.py
@@ -1,23 +1,23 @@
 from fastapi import FastAPI
 from fastapi import APIRouter
 import uvicorn
-
+from fastapi.responses import ORJSONResponse
 from app.core import config
-from .service import router
+from service import router
 
 
 router = APIRouter()
 
 
-file_upload_router = APIRouter(prefix="/file_upload", tags=["file_upload"])
-file_upload_router.include_router(router)
+#file_upload_router = APIRouter(prefix="/file_upload", tags=["file_upload"])
+#file_upload_router.include_router(router)
 
 
 app = FastAPI(
-    title=config.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api"
+    title=config.PROJECT_NAME, docs_url="/api/docs", openapi_url="/api", default_response_class=ORJSONResponse
 )
 
-app.include_router(file_upload_router)
+app.include_router(router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", reload=True, port=8000)
